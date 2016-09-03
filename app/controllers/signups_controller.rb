c93a9c6a9b34548b6ed9e5795bce3b9baeb4d0f6
Rafael Montas <rafaelmontas1@gmail.com>
2016-09-03 18:48:16 -0400

    thanks

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -2,4 +2,16 @@ class SignupsController < ApplicationController
 	def new
 		@signup = Signup.new
 	end
+	def create
+    @signup = Signup.new(signup_params) 
+  		if @signup.save 
+    		redirect_to '/thanks' 
+  		else 
+    		render 'new' 
+  		end 
+  end
+	private
+	def signup_params
+		params.require(:signup).permit(:email, :firstname)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 	get "/about" => "pages#about", as: :about
 	resources :signups
 	get "/signups/new" => "signups#new", as: :sign
+	get "/thanks" => "pages#thanks"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
